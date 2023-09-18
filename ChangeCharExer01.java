401615697a05534254818d37e6bb6101c714fd36
BetterMe9977 <lixiaowan.hh@gmail.com>
2023-09-19 00:27:49 +0900

    self

---
+++ ChangeCharExer01.java
@@ -0,0 +1,17 @@
+
+public class ChangeCharExer01 {
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		
+		//示意
+		//下面代码完成 两个数相加
+		//定义变量
+		int n1 = 10;
+		int n2 = 30;
+		//求和
+		int sum = n1 + n2;
+		//输出结果
+		System.out.println("结果=" + sum);
+	}
+}
